1394ce6d8caa0192d000e13ca5c13f90ee59b92e
endywezy <endywezy@gmail.com>
2024-07-26 04:09:00 +0000

    classes

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,38 @@
+export default class Currency {
+  constructor(code, name) {
+    if (typeof code !== 'string') {
+      throw new TypeError('Code must be a string');
+    }
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    this._code = code;
+    this._name = name;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set code(theCode) {
+    if (typeof theCode !== 'string') {
+      throw new TypeError('Code must be a string');
+    }
+    this._code = theCode;
+  }
+
+  set name(theName) {
+    if (typeof theName !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    this._name = theName;
+  }
+
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
+}
