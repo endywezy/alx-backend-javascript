83f72e8285fccf953c2ddeee6545cebaba4ad5e6
endywezy <endywezy@gmail.com>
2024-07-26 04:05:33 +0000

    classes

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,54 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    if (!Number.isInteger(length)) {
+      throw new TypeError('Length must be a number');
+    }
+    if (!Array.isArray(students)) {
+      throw new TypeError('Students must be an array');
+    }
+    this._name = name,
+    this._length = length;
+    this._students = students;
+  }
+
+  /* define the getters for the class */
+  get name() {
+    return this._name;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  /*
+    * Settters used to trip me up,
+    * or should I say, set me up
+  */
+  set name(newName) {
+    if (typeof newName !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    this._name = newName;
+  }
+
+  set length(newlength) {
+    if (typeof newlength !== 'number') {
+      throw new TypeError('Length must be a number');
+    }
+    this._length = newlength;
+  }
+
+  set students(studArray) {
+    if (!Array.isArray(studArray)) {
+      throw new TypeError('Students must be an array');
+    }
+    this._students = studArray;
+  }
+}
