25381a7416faa6315a068faf3485c95ce53f9445
endywezy <endywezy@gmail.com>
2024-07-30 06:02:15 +0100

    chomod

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,16 +1,13 @@
 function cleanSet(set, startString) {
-  // Return an empty string if startString is empty
   if (!startString || typeof startString !== 'string') {
     return '';
   }
   const result = [];
   set.forEach((value) => {
     if (value.startsWith(startString)) {
-      // Add the substring without the startString to the result array
       result.push(value.slice(startString.length));
     }
   });
-  // Join all the parts with a hyphen
   return result.join('-');
 }
 export default cleanSet;
