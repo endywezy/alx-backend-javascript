3aac9e03c7f5e264b7fa3e47a784ea3aad965b3a
endywezy <endywezy@gmail.com>
2024-09-20 23:39:12 +0100

    resolved promise with data when success is true

--- 0x06-unittests_in_js/6-payment_token.js
+++ 0x06-unittests_in_js/6-payment_token.js
@@ -1,11 +1,14 @@
+#!/usr/bin/node
+
+/**
+ * getPaymentTokenFromAPI - Returns a promise based on the success parameter.
+ * @param {boolean} success - If true, returns a resolved promise with data.
+ * @returns {Promise<object>} - A promise that resolves to an object if success is true.
+ */
 function getPaymentTokenFromAPI(success) {
-  return new Promise((resolve, reject) => {
     if (success) {
-      resolve({ data: 'Successful response from the API' });
-    } else {
-      reject();
+        return Promise.resolve({ data: "Successful response from the API" });
     }
-  });
 }
 
 module.exports = getPaymentTokenFromAPI;
