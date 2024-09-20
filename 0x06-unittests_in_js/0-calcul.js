6da3e520c7b6ad95e73f37027344a5541c7d2220
endywezy <endywezy@gmail.com>
2024-09-21 00:38:23 +0100

    Basic test with Mocha and Node assertion library

--- 0x06-unittests_in_js/0-calcul.js
+++ 0x06-unittests_in_js/0-calcul.js
@@ -1,5 +1,5 @@
-function calculateNumber(a, b) {
-  return Math.round(a) + Math.round(b);
-}
+#!/usr/bin/node
+const calculateNumber = (a, b) => Math.round(a) + Math.round(b);
 
 module.exports = calculateNumber;
+
--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,11 +1,22 @@
 const assert = require('assert');
 const calculateNumber = require('./0-calcul');
 
-describe('calculateNumber', function() {
-  it('should return the sum of rounded numbers', function() {
+describe('calculateNumber', () => {
+  it('should return 4 for (1, 3)', () => {
     assert.strictEqual(calculateNumber(1, 3), 4);
+  });
+
+  it('should return 5 for (1, 3.7)', () => {
     assert.strictEqual(calculateNumber(1, 3.7), 5);
+  });
+
+  it('should return 5 for (1.2, 3.7)', () => {
     assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+  });
+
+  it('should return 6 for (1.5, 3.7)', () => {
     assert.strictEqual(calculateNumber(1.5, 3.7), 6);
   });
+
+  // Additional edge cases can be added here
 });
