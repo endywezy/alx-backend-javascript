d9dce0b1002e1850d4f526889601f0802c5acdeb
endywezy <endywezy@gmail.com>
2024-09-21 04:15:13 +0100

    update

--- 0x06-unittests_in_js/0-calcul.js
+++ 0x06-unittests_in_js/0-calcul.js
@@ -1,13 +1,3 @@
-#!/usr/bin/node
-
-/**
- * Calculates the sum of two rounded numbers.
- * @param {number} a - The first number.
- * @param {number} b - The second number.
- * @returns {number} The sum of the rounded numbers.
- */
-const calculateNumber = (a, b) => {
-  return Math.round(a) + Math.round(b);
-};
+const calculateNumber = (a, b) => Math.round(a) + Math.round(b);
 
 module.exports = calculateNumber;
